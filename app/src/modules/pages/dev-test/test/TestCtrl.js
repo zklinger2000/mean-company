16424e53e135c7f51125a4d7f6019205ceb885d1
Zack Klinger <zklinger@gmail.com>
2015-06-28 11:44:05 -0700

    ADD test.less to show how HTML files are used in modules

--- app/src/modules/pages/dev-test/test/TestCtrl.js
+++ app/src/modules/pages/dev-test/test/TestCtrl.js
@@ -1,7 +1,8 @@
 'use strict';
 
-angular.module('myApp').controller('TestCtrl', ['$scope', '$timeout', 'appHttp', 'UserModel', '$location', function($scope, $timeout, appHttp, UserModel, $location) {
-	
+angular.module('myApp').controller('TestCtrl', ['$scope', '$timeout', 'appHttp', 'UserModel', '$location', '$q', function($scope, $timeout, appHttp, UserModel, $location, $q) {
+
+	$scope.myVar = 'var1';
 	$scope.user =UserModel.load();
 	
 	$scope.swipeIt =function(evt, direction, params) {
@@ -11,4 +12,40 @@ angular.module('myApp').controller('TestCtrl', ['$scope', '$timeout', 'appHttp',
 	$scope.tapIt =function(evt, params) {
 		console.log('tap');
 	};
+
+	/*
+	function sync(var1) {
+		console.log('sync');
+		return var1;
+	}
+
+	function asyncFunc(var1, callback) {
+		$timeout(function() {
+			console.log('timeout finished');
+			callback();
+		}, 1000);
+		console.log('timeout started');
+	}
+
+	asyncFunc(5, function() {
+		console.log('async done');
+	});
+	console.log('next line');
+*/
+
+	function asyncFuncPromise(var1) {
+		var deferred = $q.defer();
+		$timeout(function() {
+			console.log('promise timeout finished');
+			deferred.resolve();
+		}, 1000);
+		console.log('promise timeout started');
+		return deferred.promise;
+	}
+
+	asyncFuncPromise(5)
+	.then(function() {
+		console.log('promise done');
+	});
+
 }]);
