c4f0b83f01fec812ab8d2b198c11887e20b26a2c
Zack Klinger <zklinger@gmail.com>
2015-06-30 19:58:55 -0700

    UPDATE my-page's files to show basic examples of editing the html and less

--- app/src/modules/pages/my-page/MyPageCtrl.js
+++ app/src/modules/pages/my-page/MyPageCtrl.js
@@ -5,5 +5,18 @@
 
 angular.module('myApp').controller('MyPageCtrl', ['$scope',
 function($scope) {
-	
+	$scope.items = [
+		{
+			title: 'title 1'
+		},
+		{
+			title: 'title 2'
+		},
+		{
+			title: 'title 3'
+		},
+		{
+			title: 'title 4'
+		}
+	];
 }]);
